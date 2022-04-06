7c56ae0576ffcaba38857c87dd1ac6a46f3d7224
Zentae <yanistahiri560@gmail.com>
2022-04-07 01:39:49 +0200

    initial commit. ✨

---
+++ src/main/java/io/zentae/steganography/image/Drawer.java
@@ -0,0 +1,25 @@
+package io.zentae.steganography.image;
+
+import java.awt.*;
+
+public abstract class Drawer {
+
+    private final Image image;
+
+    public Drawer(Image image) {
+        this.image = image;
+    }
+
+    /**
+     * @return the {@link Image}
+     */
+    protected Image getImage() {
+        return image;
+    }
+
+    /**
+     * Draws and return the {@link Image}
+     * @return the drawn {@link Image}
+     */
+    public abstract Image draw();
+}
---
+++ src/main/java/io/zentae/steganography/patterns/DiagonalPattern.java
@@ -0,0 +1,4 @@
+package io.zentae.steganography.patterns;
+
+public class DiagonalPattern {
+}
